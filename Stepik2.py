0c72ec0ceefecc71d8a4557566f7b9743531b601
Bruce Eckel <mindviewinc@gmail.com>
2018-09-21 17:26:39 -0600

    Much better
    Titles are also passed through markdownify

--- Stepik2.py
+++ Stepik2.py
@@ -25,27 +25,28 @@ class StepikLessonID:
     def match_lesson_name(lesson_name, id):
         for md_name in markdown_names:
             if lesson_name == md_name[4:-3]:
-                return md_name, md_name[4:-3].replace("_", " ")
+                return md_name #, md_name[4:-3].replace("_", " ")
         if id == "0":  # It's a section
             for md_name in markdown_names:
                 if lesson_name in md_name:
-                    return (
-                        md_name,
-                        md_name[4:-3]
-                        .replace("_", ": ", 2)
-                        .replace("Section: ", "Section ")
-                        .replace("_", " "),
-                    )
-        return "Not found", "Not found"
+                    return md_name
+                    # return (
+                    #     md_name,
+                    #     md_name[4:-3]
+                    #     .replace("_", ": ", 2)
+                    #     .replace("Section: ", "Section ")
+                    #     .replace("_", " "),
+                    # )
+        return "Not found" #, "Not found"
 
     def __init__(self, lesson_line):
         assert lesson_line.strip(), "Line cannot be empty"
         self.lesson_name, self.id = lesson_line.split("=")
-        self.md_name, self.title = StepikLessonID.match_lesson_name(
-            self.lesson_name, self.id
-        )
+        self.md_name = StepikLessonID.match_lesson_name(self.lesson_name, self.id)
         if self.md_name.endswith(".md"):
-            self.internal_title = (atomickotlin / self.md_name).read_text().splitlines()[0].strip()
+            self.title = (atomickotlin / self.md_name).read_text().splitlines()[0].strip()
+        else:
+            self.title = "Not Found"
 
     def __repr__(self):
         return str(self.__dict__)
@@ -59,11 +60,6 @@ if str(request.status_code).startswith("4"):
 lesson_ids = [
     StepikLessonID(line) for line in request.text.strip().splitlines() if line.strip()
 ]
-
-stepik_titles = set(s.title for s in lesson_ids)
-markdown_titles = set(md.replace("_", " ") for md in markdown_names)
-# pprint.pprint(markdown_titles - stepik_titles)
-
 
 class StepikEncoder(json.JSONEncoder):
     def default(self, obj):
