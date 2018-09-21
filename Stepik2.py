a425bbf5f3f45efb500e006fb80c7cdac19cb461
Bruce Eckel <mindviewinc@gmail.com>
2018-09-21 16:59:37 -0600

    Works, but suboptimal

--- Stepik2.py
+++ Stepik2.py
@@ -16,25 +16,36 @@ url = "https://raw.githubusercontent.com/JetBrains/AtomicKotlin/master/resources
 githome = Path(os.environ["GIT_HOME"])
 atomickotlin = githome / "AtomicKotlin" / "MarkDown"
 hugoData = githome / "AtomicKotlin-hugo" / "data"
-markdown = set(md.name[4:-3] for md in atomickotlin.glob("*.md"))
+# markdown_names = set(md.name[4:-3] for md in atomickotlin.glob("*.md"))
+markdown_names = set(md.name for md in atomickotlin.glob("*.md"))
 
 
 class StepikLessonID:
-    def __init__(self, line):
-        assert line.strip(), "Line cannot be empty"
-        _name, self.id = line.split("=")
-        if self.id == "0":  # It's a section
-            for md in markdown:
-                if _name in md:
-                    self.name = (
-                        md.replace("_", ": ", 2)
+    @staticmethod
+    def match_lesson_name(lesson_name, id):
+        for md_name in markdown_names:
+            if lesson_name == md_name[4:-3]:
+                return md_name, md_name[4:-3].replace("_", " ")
+        if id == "0":  # It's a section
+            for md_name in markdown_names:
+                if lesson_name in md_name:
+                    return (
+                        md_name,
+                        md_name[4:-3]
+                        .replace("_", ": ", 2)
                         .replace("Section: ", "Section ")
-                        .replace("_", " ")
+                        .replace("_", " "),
                     )
-        elif _name in markdown:
-            self.name = _name.replace("_", " ")
-        else:
-            self.name = "Not found"
+        return "Not found", "Not found"
+
+    def __init__(self, lesson_line):
+        assert lesson_line.strip(), "Line cannot be empty"
+        self.lesson_name, self.id = lesson_line.split("=")
+        self.md_name, self.title = StepikLessonID.match_lesson_name(
+            self.lesson_name, self.id
+        )
+        if self.md_name.endswith(".md"):
+            self.internal_title = (atomickotlin / self.md_name).read_text().splitlines()[0].strip()
 
     def __repr__(self):
         return str(self.__dict__)
@@ -49,9 +60,10 @@ lesson_ids = [
     StepikLessonID(line) for line in request.text.strip().splitlines() if line.strip()
 ]
 
-stepik_names = set(s.name for s in lesson_ids)
-markdown_names = set(md.replace('_', ' ') for md in markdown)
-pprint.pprint(markdown_names - stepik_names)
+stepik_titles = set(s.title for s in lesson_ids)
+markdown_titles = set(md.replace("_", " ") for md in markdown_names)
+# pprint.pprint(markdown_titles - stepik_titles)
+
 
 class StepikEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -66,67 +78,3 @@ if not hugoData.exists():
 (hugoData / "atomNames.json").write_text(
     json.dumps(lesson_ids, cls=StepikEncoder, indent=2)
 )
-
-# def test(name):
-#     for sname, sid  in stepikLessonIDs.items():
-#         if sname == name:
-#             return sname, sid, sname
-#         if name.startswith("Section_") and name.endswith(sname):
-#             return name, sid, sname
-#     return False, False, False
-
-# with Path('InStepik.html').open(mode='w') as f:
-#     for name in markdown:
-#         inStepik, sid, _ = test(name[1])
-#         if inStepik:
-#             print(f"{name[0]} : {inStepik} -> {sid}<br>", file=f)
-#         else:
-#             print(f'<span style="background-color: red">{name[0]} not in Stepik</span><br>', file=f)
-
-# mdNames = set([md[1] for md in markdown])
-
-# lessonIDs = OrderedDict()
-
-# for k, v in stepikLessonIDs.items():
-#     if v == "0": # It's a section
-#         for n in mdNames:
-#             if k in n:
-#                 n = n.replace('_', ': ', 2)
-#                 n = n.replace('Section: ', 'Section ')
-#                 n = n.replace('_', ' ')
-#                 lessonIDs[n] = "0"
-#     elif k in mdNames:
-#         lessonIDs[k.replace('_', ' ')] = v
-
-
-# class Stepik:
-#     def __init__(self, name, id):
-#         self.name = name
-#         self.id = id
-#         self.foo = "bar"
-#     def json(self):
-#         return f'    {{ "name": "{self.name}",  "id": "{self.id}" }},\n'
-
-
-# class StepikJSONList:
-#     def __init__(self, item_list):
-#         self.item_list = item_list
-#         self.json_list = "[\n"
-#         for item in item_list:
-#           self.json_list += item.json()
-#     def __str__(self):
-#         result = self.json_list.rstrip().rstrip(',')
-#         return result + "\n]"
-#     def json(self):
-#         for item in self.item_list:
-#             print(json.dumps(item.__dict__))
-
-# LessonIDList = StepikJSONList([Stepik(nm, id) for nm, id in lessonIDs.items()])
-# # print(LessonIDList)
-
-# if not hugoData.exists():
-#     hugoData.mkdir()
-# atoms = hugoData / "atomNames.json"
-# atoms.write_text(str(LessonIDList))
-
-# LessonIDList.json()
